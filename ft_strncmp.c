bc38121d362acc7a8762e66faf17fb0a31ba9937
shimizu shinnosuke <shinnosuke@s-mba.local>
2023-01-14 08:30:40 +0900

    improved readability

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sshimizu <sshimizu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 09:06:06 by sshimizu          #+#    #+#             */
-/*   Updated: 2023/01/14 01:50:39 by sshimizu         ###   ########.fr       */
+/*   Updated: 2023/01/14 08:28:14 by sshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned int	i;
 
 	i = 0;
-	while (i < n - 1)
+	while (i < n)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
@@ -25,7 +25,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
 
 // #include <stdio.h>
@@ -40,5 +40,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 //     printf("ABC: AB    = %d\n", ft_strncmp(str, "AB", 2));
 //     printf("ABC: B     = %d\n", ft_strncmp(str, "B", 2));
 //     printf("ABC: A     = %d\n", ft_strncmp(str, "A", 2));
+// 	printf("\n");
+// 	printf("ABC: ABD   = %d\n", strncmp(str, "ABD", 2));
+//     printf("ABC: ABC   = %d\n", strncmp(str, "ABC", 2));
+//     printf("ABC: AAA   = %d\n", strncmp(str, "AAA", 2));
+//     printf("ABC: ABCD  = %d\n", strncmp(str, "ABCD", 2));
+//     printf("ABC: AB    = %d\n", strncmp(str, "AB", 2));
+//     printf("ABC: B     = %d\n", strncmp(str, "B", 2));
+//     printf("ABC: A     = %d\n", strncmp(str, "A", 2));
 //     return (0);
 // }
